652819b7d95d2e7fd586a5a55652064f7b24faee
vagrant <lovemore_jokonya@yahoo.com>
2017-11-16 23:23:38 +0000

    get array directly

--- mongo_example.js
+++ mongo_example.js
@@ -13,20 +13,35 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   //     starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  // ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
-  db.collection("tweets").find({}, (err, result) => {
-    // Lazy error handling:
+  // ==> We can just get the results as an array all at once:
+  db.collection("tweets").find().toArray((err, results) => {
     if (err) throw err;
 
-    // ==> Fair warning: This is going to log a lot of stuff...
-    console.log("find result: ", result);
-    console.log("type of find result: ", typeof result);
+    console.log("results array: ", results);
 
-    // ==> This is inside this callback now. Think about it:
-    // This is now the "end of the program", right?.
+    // This is the end...
+    db.close();
+  });
 
-  // ==> At the end, we close the connection:
-  db.close();
-});
+  // ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
+//   db.collection("tweets").find({}, (err, results) => {
+//     // Lazy error handling:
+//     if (err) throw err;
+
+//     // ==> So we Read The Fantastic Manual, right?
+
+//     // ==> We can iterate on the cursor to get results, one at a time:
+//     console.log("for each item yielded by the cursor:");
+//     // results.each((err, item) => console.log("  ", item));
+//     // ==> We could instead just slurp the items into an array:
+//     results.toArray((err, resultsArray) => {
+//       if (err) throw err;
+
+//       console.log("results.toArray:", resultsArray);
+//     });
+
+//     // This is the end...
+//   db.close();
+// });
 
 });
