f64399bc1d407086142ec36e77af9a64eafc6a2c
vagrant <lovemore_jokonya@yahoo.com>
2017-11-16 16:49:58 +0000

    implemented prevent default on tweet submission

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -15,13 +15,19 @@ $( document ).ready(function() {
 
   });
 
+  // if user hovers over a tweet it should display the icons and change border color to black
+  $("article").hover(function () {
+    $(this).find(".icons").show();
+    $(this).css("border-color", "black");
+  },function () {  // else it should hide the icons and change borader color to grey
+      $(this).find(".icons").hide();
+      $(this).css("border-color", "lightgrey");
+  });
 
-      $("article").hover(function () {
-        $(this).find(".icons").show();
-        $(this).css("border-color", "black");
-    },function () {
-        $(this).find(".icons").hide();
-        $(this).css("border-color", "lightgrey");
-    });
+// prevent default submission of form and redirection
+$(this).click(function( event ) {
+  event.preventDefault();
+  console.log ( "default " + event.type + " prevented" );
+});
 
 });
