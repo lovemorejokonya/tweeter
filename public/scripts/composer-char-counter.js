3fac74b452d5b7d377cfcb5fefc8bffb9494368f
vagrant <lovemore_jokonya@yahoo.com>
2017-11-16 19:50:36 +0000

    implemented form validation for tweet submission

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -27,18 +27,46 @@ $( document ).ready(function() {
 // prevent default submission of form and redirection
 $("#tweetForm").submit(function( event ) {
   event.preventDefault();
-  // console.log ( "default " + event.type + " prevented" );
-  const tweetData = $("#tweetForm").serialize();
-  // console.log(tweetData);
-  // do post request using ajax
-  $.ajax({
-              url: "/tweets",
-              type: "post",
-              data: tweetData,
-              success: function(d) {
-                  console.log(d);
-              }
-          });
+  // error handling
+  // console.log($(this).find("textarea").val());
+  // if ($(this).find("textarea").val() = ""){
+  //   $errorMessage = $("<div>").text(object.content.text);
+  //   $content = $("new-tweet").append($errorMessage);
+  // }
+  if($("textarea").val().length == 0 || $("textarea").val().length > 2){
+    validate();
+  } else {
+    //clear errorMessage HTMNL
+    errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerHTML = "";
+    const tweetData = $("#tweetForm").serialize();
+    // do post request using ajax
+    $.ajax({
+                url: "/tweets",
+                type: "post",
+                data: tweetData,
+                success: function(d) {
+                    console.log(d);
+                }
+            });
+    }
 });
+
+function validate() {
+    var errorMessage, x;
+    errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerHTML = "";
+    x = $(this).find("textarea").val();
+    try {
+        if($("textarea").val().length == 0)  throw "You can not submit an empty tweet. Please enter something!";
+        if($("textarea").val().length > 2) throw "You can not submit more than 140 characters";
+        // x = Number(x);
+        // if(x < 5)    throw "too low";
+        // if(x > 10)   throw "too high";
+    }
+    catch(err) {
+        errorMessage.innerHTML = err;
+    }
+}
 
 });
